49ffdf22323167abd8c56b9f6b97b3c752ac31be
Paul Bomba <paul@paulbomba.com>
2018-05-30 17:12:25 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -83,8 +83,8 @@ def adding_matz
         }
      }
 
-    programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby"}
-
+    programmer_hash[:yukihiro_matsumoto] = {}
+    return programmer_hash
 end
 
 def changing_alan
