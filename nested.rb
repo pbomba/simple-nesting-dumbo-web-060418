9a07696f18084e1e1dcd3b1dd173c1a6165e643c
Paul Bomba <paul@paulbomba.com>
2018-05-30 17:16:49 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -127,5 +127,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages][].push("Assembly")
+arr = programmer_hash[:dennis_ritchie][:languages]
+arr.push("Assembly")
+programmer_hash[:dennis_ritchie][:languages] = arr
 end
